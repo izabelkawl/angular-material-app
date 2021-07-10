4a9adfb27978f4f34f93b358645466cd32c6e831
Iza <izabelawlazlo9@gmail.com>
2021-07-10 16:42:05 +0200

    simple routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './shared/components/home/home.component';
+import { LoginComponent } from './shared/components/login/login.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
+		HomeComponent,
+		LoginComponent,
 	],
 	imports: [
 		BrowserModule,
