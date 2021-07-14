79b5cba2f6772477c136f8b051d5d0d682b1e611
Iza <izabelawlazlo9@gmail.com>
2021-07-14 21:57:06 +0200

    paginator

--- src/app/shared/components/filter/filter.component.ts
+++ src/app/shared/components/filter/filter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 
 export interface PeriodicElement {
@@ -46,19 +47,24 @@ const ELEMENT_DATA: PeriodicElement[] = [
 	templateUrl: './filter.component.html',
 	styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent  implements AfterViewInit {
 
 	// constructor() { }
 	displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-	dataSource = new MatTableDataSource(ELEMENT_DATA);
-
+	dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+	@ViewChild(MatPaginator) paginator!: MatPaginator;
 	applyFilter(event: Event) {
 		const filterValue = (event.target as HTMLInputElement).value;
 		this.dataSource.filter = filterValue.trim()
 		.toLowerCase();
 	}
+	
+	ngAfterViewInit() {
+		this.dataSource.paginator = this.paginator;
+	  }
+	
 
-	ngOnInit(): void {
-	}
+	// ngOnInit(): void {
+	// }
 
 }
